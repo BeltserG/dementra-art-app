8af44a5933c88655a311e55866a350b3b1e14e4f
BeltserG <beltserg@icloud.com>
2023-09-04 19:44:32 +0500

    Added head content

--- components/widgets/home/HomeGallerySection/HomeGallerySection.jsx
+++ components/widgets/home/HomeGallerySection/HomeGallerySection.jsx
@@ -1,11 +1,10 @@
 import GalleryCard from "./components/GalleryCard"
 import gallery from "@/data/gallery.json";
 const HomeGallerySection = () => {
-  console.log(gallery)
   return (
     <section>
         <h2></h2>
-        {gallery.map(section=> <GalleryCard data={section}/>)}
+        {gallery.map(section=> <GalleryCard key={section.id} data={section}/>)}
     </section>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import MainLayout from "@/components/Layout/MainLayout";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <MainLayout>
+      <Head>
+        <title>Dementra Art</title>
+        <meta
+          name="description"
+          content="Welcome to Dementra Art Gallery! 2D Art and Environmental Design, Oil Painting, Glass Art"
+        />
+        <meta httpEquiv="Content-Type" content="text/html" charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
     </MainLayout>
   );
