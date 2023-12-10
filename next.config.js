52646fffc28cae3bfb3a253f4a20c548b8b43c55
BeltserG <beltserg@icloud.com>
2023-12-11 01:59:28 +0500

    sitemap and robots

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ['gsap'],
+module.exports = {
   reactStrictMode: true,
+  transpilePackages: ["gsap"],
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'all',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  }
 }
-
-module.exports = nextConfig
